0f79ec64f48c18db9823e9adf68688fdbbe0d7ff
ziemlaur <ziemlaur@gmail.com>
2024-10-15 20:13:04 +0300

    3 paskaitos uzduotis

--- main.py
+++ main.py
@@ -1,1 +1,25 @@
-print('hey')
+print(len('Laura'))    
+print(len('Žiemelytė'))
+
+print(len('Žiniasklaida: po susitikimo su lenkų diplomatu Zelenskis liko įsiutęs'))
+
+
+vardas = 'Laura'
+print(vardas.lower())
+print(vardas.upper())
+
+delfi = 'Žiniasklaida: po susitikimo su lenkų diplomatu Zelenskis liko įsiutęs'
+print(delfi[0:10])
+print(delfi[-11:])
+print(delfi[1::2])
+print(delfi[9:19])
+
+print(delfi.count('a'))
+print(delfi.find('a'))
+
+txt = 'Labas rytas!'
+print(txt[1:])
+print(txt[:-1])
+print(txt[1:-1])
+
+
