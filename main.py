f0c55e216bd9e6249b3519bdfbf52360cef6df06
ziemlaur <ziemlaur@gmail.com>
2024-10-15 21:32:39 +0300

    3 paskaitos papildomi darbai

--- main.py
+++ main.py
@@ -22,4 +22,28 @@ print(txt[1:])
 print(txt[:-1])
 print(txt[1:-1])
 
+#5 uzduotis
+text = 'An American in Paris'
+print(text.replace('a','*').replace('A','*'))
+#6 uzduotis
+s=text.count('a')
+a=text.count('A')
+print(a+s)
+#7 uzduotis
+textKitas = 'Breakfast at Tiffany\'s'
+textDarKitas = '2001: A Space Odyssey'
+textVelKitas = 'It\'s a Wonderful Life'
+
+vowels = 'AEIOUaeiou'
+mytable = str.maketrans('','',vowels)
+print(text.translate(mytable))
+
+mytable = str.maketrans('','',vowels)
+print(textKitas.translate(mytable))
+
+mytable = str.maketrans('','',vowels)
+print(textDarKitas.translate(mytable))
+
+mytable = str.maketrans('','',vowels)
+print(textVelKitas.translate(mytable))
 
